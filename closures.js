2ee665f64250d41a97c862d8f8d33b516ac2c8f9
Nick <pickme1152019@gmail.com>
2019-03-07 23:13:58 +0000

    first commit

---
+++ closures.js
@@ -0,0 +1,17 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  let i = 0;
+
+  return function() {
+    /* your code here */
+
+    return list[i++]
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
