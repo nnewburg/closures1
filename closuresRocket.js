795a3860d69c3d39fea32d9cebb6f2f601d324b0
Nick <pickme1152019@gmail.com>
2019-03-07 23:32:06 +0000

    first commit

---
+++ closuresRocket.js
@@ -0,0 +1,28 @@
+var countdownGenerator = function (x) {
+  /* your code here */
+  let countdown = x;
+
+  return function(){
+
+    if(x > 0){
+       console.log(`T-minus ${x}...`)
+       x--
+    } else if(x == 0 ){
+       console.log("Blast Off!")
+       x--;
+    } else{
+      console.log("Rockets already gone, bub!")
+    }
+
+
+  }
+
+};
+
+var countdown = countdownGenerator(10);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
